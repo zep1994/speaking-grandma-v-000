6e774ddd581a5f7da1db35c89d183e6c82f97e16
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-04-08 18:05:56 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,10 +3,6 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif  phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  if phrase.upcase
-    return "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
   else
     return "HUH?! SPEAK UP, SONNY!"
 

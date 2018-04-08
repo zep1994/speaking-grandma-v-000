c2bf21d66d841dcd3e8e3d7b4f04ab294cd460fa
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-04-08 18:04:14 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,16 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
+<<<<<<< HEAD
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif  phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
+=======
+  if phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+>>>>>>> a6d88f423d08c2ba8d8a4b298c48b5d06ca2d582
   else
     return "HUH?! SPEAK UP, SONNY!"
 
